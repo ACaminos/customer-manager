ddf0181c471f3bde36ed097fc457d72ea6dc9e8d
Ariel <caminosariel@gmail.com>
2022-06-22 20:29:11 -0300

    change in VerCliente ... add conditionals

--- crm-react/src/paginas/VerCliente.jsx
+++ crm-react/src/paginas/VerCliente.jsx
@@ -22,43 +22,47 @@ const VerCliente = () => {
         obtenerClienteAPI()
     },[])
   return (
-    <div>
-        <h1 className='font-black text-4xl text-blue-900'>Ver cliente: {cliente.nombre}</h1>
-        <p className='mt-3'>Informacion del cliente</p>
-
-        {cliente.nombre && (
-        <p className="text-2xl text-gray-600 mt-10">
-            <span className="uppercase font-bold">Cliente: </span>
-            {cliente.nombre}
-        </p>
-        )}
-
-        {cliente.email && (
-            <p className="text-2xl text-gray-600 mt-4">
-                <span className="text-gray-800 uppercase font-bold">Email: </span>
-                {cliente.email}
-            </p>
-        )}
-
-        {cliente.telefono && (
-            <p className="text-2xl text-gray-600 mt-4">
-                <span className="text-gray-800 uppercase font-bold">Telefono: </span>
-                {cliente.telefono}
-            </p>
-        )}
-
-        {cliente.empresa && (
-            <p className="text-2xl text-gray-600 mt-4">
-                <span className="text-gray-800 uppercase font-bold">Empresa: </span>
-                {cliente.empresa}
-            </p>
-        )}
-
-        {cliente.notas && (
-            <p className="text-2xl text-gray-600 mt-4">
-                <span className="text-gray-800 uppercase font-bold">Notas: </span>
-                {cliente.notas}
-            </p>
+      <div>
+        {cargando ? 'cargando...' : (
+            <>
+                <h1 className='font-black text-4xl text-blue-900'>Ver cliente: {cliente.nombre}</h1>
+                <p className='mt-3'>Informacion del cliente</p>
+        
+                {cliente.nombre && (
+                <p className="text-2xl text-gray-600 mt-10">
+                    <span className="uppercase font-bold">Cliente: </span>
+                    {cliente.nombre}
+                </p>
+                )}
+        
+                {cliente.email && (
+                    <p className="text-2xl text-gray-600 mt-4">
+                        <span className="text-gray-800 uppercase font-bold">Email: </span>
+                        {cliente.email}
+                    </p>
+                )}
+        
+                {cliente.telefono && (
+                    <p className="text-2xl text-gray-600 mt-4">
+                        <span className="text-gray-800 uppercase font-bold">Telefono: </span>
+                        {cliente.telefono}
+                    </p>
+                )}
+        
+                {cliente.empresa && (
+                    <p className="text-2xl text-gray-600 mt-4">
+                        <span className="text-gray-800 uppercase font-bold">Empresa: </span>
+                        {cliente.empresa}
+                    </p>
+                )}
+        
+                {cliente.notas && (
+                    <p className="text-2xl text-gray-600 mt-4">
+                        <span className="text-gray-800 uppercase font-bold">Notas: </span>
+                        {cliente.notas}
+                    </p>
+                )}
+          </>  
         )}
     </div>
   )
