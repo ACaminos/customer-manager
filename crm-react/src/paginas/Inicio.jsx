16849a533b871236f43c052edffbf0c32b5de362
Ariel <caminosariel@gmail.com>
2022-06-27 21:07:33 -0300

    finish delete

--- crm-react/src/paginas/Inicio.jsx
+++ crm-react/src/paginas/Inicio.jsx
@@ -21,6 +21,27 @@ const Inicio = () => {
 
     obtenerClientesAPI()
   }, [])
+
+  const handleEliminar = async id => {
+    const confirmar = confirm('Deseas eliminar este cliente?')
+    
+    if(confirmar){
+      try{
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url, {
+          method: 'DELETE'
+        })
+        await respuesta.json()
+
+        const arrayClientes = clientes.filter(cliente => cliente.id !== id)
+        setClientes(arrayClientes)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    
+  }
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Cliente</h1>
@@ -40,6 +61,7 @@ const Inicio = () => {
             <Cliente
               key={cliente.id}
               cliente={cliente}
+              handleEliminar={handleEliminar}
             />
 
           ))}
